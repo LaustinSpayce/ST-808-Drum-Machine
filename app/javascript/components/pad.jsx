c12fb5d7d36b787ff4bbcd6995587defe1e4f445
Stuart Myers <stuart.myers@gmail.com>
2020-03-05 18:29:03 +0800

    I don't think I want every pad updating every tick

--- app/javascript/components/pad.jsx
+++ app/javascript/components/pad.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import Tone from 'tone'
 import Box from '@material-ui/core/Box'
 import { styled, withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
 
-const PadBox = styled(Box)({
+const PadBox = styled(Button)({
   display: 'inline-block',
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   border: 0,
