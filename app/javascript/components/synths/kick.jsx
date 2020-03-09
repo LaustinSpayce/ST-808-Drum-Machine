2e67adeb0e62d562d9555a95b5a127115c7e8838
Stuart Myers <stuart.myers@gmail.com>
2020-03-09 11:57:59 +0800

    Can change volume level of kick

--- app/javascript/components/synths/kick.jsx
+++ app/javascript/components/synths/kick.jsx
@@ -7,6 +7,9 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import Slider from '@material-ui/core/Slider'
+import Grid from '@material-ui/core/Grid'
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -28,10 +31,14 @@ export default class Kick extends Component {
     this.state = {
       tone: 167.1,
       decay: 0.02,
-      volume: 1,
+      volume: 0,
       minTone: 41
     }
 
+    this.adjustTone = this.adjustTone.bind(this)
+    this.adjustVolume = this.adjustVolume.bind(this)
+    this.triggerKickSynth = this.triggerKickSynth.bind(this)
+  
     this.kickSynth = new Tone.MonoSynth( { oscillator: {type: 'sine'}}).toMaster()
     this.kickSynth.fadeOut = 0.01
     this.frequencyEnvelope = new Tone.ScaledEnvelope( { attack: 0.001, decay: this.state.decay, sustain: 1, release: 0.01 })
@@ -39,6 +46,17 @@ export default class Kick extends Component {
     this.frequencyEnvelope.max = this.state.tone
     this.frequencyEnvelope.sustain = 0.01
     this.frequencyEnvelope.connect(this.kickSynth.frequency)
+    this.kickSynth.volume.value = this.state.volume
+
+    this.volumeMarks = [
+      {value: -32, label: '-32dB'},
+      {value: 0, label: '0dB'},
+      {value: 6, label: '+6dB'}]
+
+    this.toneMarks = [
+      {value: -32, label: '-32dB'},
+      {value: 0, label: '0dB'},
+      {value: 6, label: '+6dB'}]
   }
 
   triggerKickSynth(time, value) {
@@ -48,8 +66,20 @@ export default class Kick extends Component {
     }
   }
 
+  adjustVolume(event, value) {
+    let newVolume = Math.floor(value)
+    this.setState( { volume: newVolume } )
+    this.kickSynth.volume.value = newVolume
+  }
+
+  adjustTone(event, value) {
+    let newTone = value
+    this.setState( { tone: newTone } )
+    this.frequencyEnvelope.max = newTone
+  }
+
   render() {
-    return (
+    return (  
       <ExpansionPanel>
         <ExpansionPanelSummary
           expandIcon={<ExpandMoreIcon />}
@@ -63,7 +93,27 @@ export default class Kick extends Component {
             onFocus={event => event.stopPropagation()}>Boom</Button>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          Here are the settings for Boom Boom Pow
+            <Typography id="volume-slider" gutterBottom>
+              Volume: {this.state.volume}
+            </Typography>
+            <Slider
+              aria-labelledby="volume-slider"
+              value={this.state.volume}
+              scale={x => x ** 6}
+              max={6}
+              min={-32}
+              marks={this.volumeMarks}
+              onChange={this.adjustVolume} /><br/>
+            <Typography id="tone-slider" gutterBottom>
+              Tone Frequency: {this.state.tone}
+            </Typography>
+            <Slider
+              aria-labelledby="tone-slider"
+              value={this.state.tone}
+              max={200}
+              min={100}
+              marks={this.toneMarks}
+              onChange={this.adjustTone} /><br/>
         </ExpansionPanelDetails>
       </ExpansionPanel>
     )
