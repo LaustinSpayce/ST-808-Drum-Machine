98016b453c9227ee1c4fb749be1f92365d8ef1fa
Stuart Myers <stuart.myers@gmail.com>
2020-03-10 17:07:33 +0800

    Refactored pad and state-based CSS

--- app/javascript/components/pad.jsx
+++ app/javascript/components/pad.jsx
@@ -1,43 +1,70 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Tone from 'tone'
 import Box from '@material-ui/core/Box'
-import { styled, withStyles } from '@material-ui/core/styles'
-import { palette } from '@material-ui/system';
+import { makeStyles } from '@material-ui/core/styles';
 
-const PadBox = styled(Box)({
-  display: 'inline-block',
-  border: '2px solid black',
-  borderRadius: 3,
-  color: 'black',
-  height: 24,
-  width: 24,
-  padding: '2px',
-  margin: '4px'
+// this is in ms, not seconds!
+const PAD_LIGHT_DURATION = 100
+
+const useStyles = makeStyles({
+  root: {
+    display: 'inline-block',
+    background: props =>
+      props.onBeat ? 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)' :
+      props.active ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+        : 'linear-gradient(45deg, #444444 30%, #777777 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: props =>
+      props.onBeat ? '0 3px 5px 2px rgba(33, 203, 243, .3)' :
+      props.active ? '0 3px 5px 2px rgba(255, 105, 135, .3)' 
+        : '0 3px 5px 2px rgba(105, 105, 105, .3)',
+    color: 'white',
+    height: 32,
+    width: 32,
+    margin: '0px 4px'
+  },
+  active: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
+  },
+  onBeat: {
+    background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+    boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)'
+  }
 });
 
-export default class Pad extends React.Component {
-  constructor(props) {
-    super(props)
+export default function Pad(props) {
+  const [active, setActive] = useState(false)
+  const [onBeat, setOnBeat] = useState(false)
 
-    this.state = {
-      active: false,
-    }
-
-    this.onClick = this.onClick.bind(this)
-  }
-
-  onClick() {
-    let state = this.state.active
+  function onClick() {
+    let state = active
     state = !state
-    this.setState({active: state})
-    this.props.clicked()
+    setActive(state)
+    props.clicked()
   }
 
-  render () {
-    let color = this.props.currentTick === this.props.index ? "success.main" : "text.secondary"
-    color = this.state.active === true ? "warning.main" : color
-    return (
-      <PadBox onClick={() => {this.onClick()}} bgcolor={color}></PadBox>
-    )
+  function lightPad() {
+    setOnBeat(true)
+    setTimeout(turnOffPad, PAD_LIGHT_DURATION)
   }
+
+  function turnOffPad() {
+    setOnBeat(false)
+  }
+
+  useEffect(() => {
+    Tone.Transport.scheduleRepeat((time) => {Tone.Draw.schedule(lightPad, time)}, '1m', props.timing)
+  }, [])
+
+  const propsStyle = { active: active, onBeat: onBeat }
+
+  const classes = useStyles(propsStyle)
+
+  return (
+    <Box 
+    onClick={() => {onClick()}}
+    className={classes.root}></Box>
+  )
 }
