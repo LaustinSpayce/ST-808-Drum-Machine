4c4b8439db296c232b8984cd464f28c893e45f7d
Stuart Myers <stuart.myers@gmail.com>
2020-03-11 15:12:18 +0800

    Scaffold for saving songs to backend

---
+++ app/views/songs/_song.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! song, :id, :song, :created_at, :updated_at
+json.url song_url(song, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :songs
   get 'machine/machine'
   root to: 'machine#machine'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
